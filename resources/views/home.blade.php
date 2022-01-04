7451322279e20c7b7e6e66cb1cd36d97ff60fbe3
felipe cruz <felipephplow@gmail.com>
2022-01-04 12:26:29 -0300

    Novas alt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,5 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header text-center">GM BRASIL</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Olá, {{ auth()->user()->name }} <br>
-                    Seu código é: <span class="text-success">{{ auth()->user()->uniqid }}</span> <br>
-                    <br>
-                    Estamos trabalhando para você, tudo vai ficar certinho tenha paciência. <br>
-                    <br>
-                    Toda grande jornada começa com um simples passo!<br>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
