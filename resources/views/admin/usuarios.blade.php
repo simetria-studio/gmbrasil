baf6cb6f579bc579ee92ab62215230de64f0e682
felipe cruz <felipephplow@gmail.com>
2022-01-10 16:58:57 -0300

    Feat

--- resources/views/admin/usuarios.blade.php
+++ resources/views/admin/usuarios.blade.php
@@ -7,7 +7,7 @@
             <div class="iq-card">
                 <div class="iq-card-header d-flex justify-content-between">
                     <div class="iq-header-title">
-                        <h4 class="card-title">Editable Table</h4>
+                        <h4 class="card-title">Usuários</h4>
                     </div>
                 </div>
                 <div class="iq-card-body">
