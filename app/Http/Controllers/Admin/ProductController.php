25a65fb20f2ef9ea9e6bfcbb8504b55ccb589bd0
Felipe cruz <felipephplow@gmail.com>
2022-01-17 17:31:17 -0300

    Subir

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 // use Intervention\Image\Image;
 use Intervention\Image\ImageManagerStatic as Image;
 use App\Models\ProductCategory;
+use Intervention\Image\ImageManagerStatic;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Controllers\Controller;
 
@@ -147,6 +148,7 @@ class ProductController extends Controller
 
     public function atualizarProduto(Request $request)
     {
+        // return response()->json('oi');
         $request->validate([
             'code'              => 'unique:products,code,' . $request->id,
             'name'              => 'required|string',
@@ -157,6 +159,10 @@ class ProductController extends Controller
         ]);
 
         $originalPath = storage_path('/app/product_image/');
+        if (!file_exists($originalPath)) {
+            mkdir($originalPath, 0777, true);
+        }
+
 
         $products['code']               = $request->code;
         $products['name']               = mb_convert_case($request->name, MB_CASE_TITLE);
@@ -176,73 +182,51 @@ class ProductController extends Controller
 
         $products = Product::where('id', $request->id)->update($products);
 
-        // ProductAttribute::where('product_id', $request->id)->delete();
-
-        // foreach ($request->attribute as $attr) {
-        //     $attr = json_decode(json_encode($attr));
-        //     if ($attr->icheck_attribute == 'S') {
-        //         $attributes['product_id']       = $request->id;
-        //         $attributes['parent_id']        = $attr->parent_id;
-        //         $attributes['attribute_id']     = $attr->attribute_id ?? null;
-        //         $attributes['attribute_name']   = $attr->attribute_name ?? null;
-        //         $attributes['attribute_value']  = $attr->attribute_value ?? '' ? str_replace(['.', ','], ['', '.'], $attr->attribute_value) : null;
-
-        //         ProductAttribute::create($attributes);
-        //     }
-        // }
 
         if ($request->img_principal) {
-            $width_max = 420;
-            $height_max = 480;
 
-            list($width_orig, $height_orig) = getimagesize($request->img_principal);
-            $ratio_orig = $width_orig / $height_orig;
-            if ($width_max / $height_max > $ratio_orig) {
-                $width_max = $height_max * $ratio_orig; //----
-            } else {
-                $height_max = $width_max / $ratio_orig; //----
-            }
-            $img_principal = Image::make($request->img_principal)->resize($width_max, $height_max);
-            $img_name = Str::random() . '.' . $request->img_principal->extension();
-            $img_principal->save($originalPath . $img_name);
+            $imageName = time() . '.' . $request->img_principal->extension();
+
+            $request->img_principal->move(storage_path('/app/product_image/'), $imageName);
 
             $img_update_principal = ProductImage::where('product_id', $request->id)->where('sequence', '1')->first();
-            Storage::delete($img_update_principal->image_name);
+            // Storage::delete($img_update_principal->image_name);
 
-            $create_img_principal['image_name'] = $img_principal;
+            $create_img_principal['image_name'] = 'product_image/' . $imageName;
             $img_update_principal->update($create_img_principal);
         }
 
-        if ($request->img_multipla) {
-            $img_deleta_multiplas = ProductImage::where('product_id', $request->id)->where('sequence', '>=', '2')->get();
-            foreach ($img_deleta_multiplas as $imgdm) {
-                Storage::delete($imgdm->image_name);
-                ProductImage::where('id', $imgdm->id)->delete();
-            }
 
-            foreach ($request->img_multipla as $for_img_multipla) {
-                $count_sequence = ProductImage::where('product_id', $request->id)->orderByDesc('sequence')->first();
+        // if ($request->img_multipla) {
+        //     $img_deleta_multiplas = ProductImage::where('product_id', $request->id)->where('sequence', '>=', '2')->get();
+        //     foreach ($img_deleta_multiplas as $imgdm) {
+        //         Storage::delete($imgdm->image_name);
+        //         ProductImage::where('id', $imgdm->id)->delete();
+        //     }
 
-                $width_max = 420;
-                $height_max = 480;
+        //     foreach ($request->img_multipla as $for_img_multipla) {
+        //         $count_sequence = ProductImage::where('product_id', $request->id)->orderByDesc('sequence')->first();
 
-                list($width_orig, $height_orig) = getimagesize($for_img_multipla);
-                $ratio_orig = $width_orig / $height_orig;
-                if ($width_max / $height_max > $ratio_orig) {
-                    $width_max = $height_max * $ratio_orig; //----
-                } else {
-                    $height_max = $width_max / $ratio_orig; //----
-                }
-                $img_multipla = Image::make($for_img_multipla)->resize($width_max, $height_max);
-                $img_name_m = Str::random() . '.' . $for_img_multipla->extension();
-                $img_multipla->save($originalPath . $img_name_m);
+        //         $width_max = 420;
+        //         $height_max = 480;
 
-                $create_img_multipla['product_id'] = $request->id;
-                $create_img_multipla['sequence']   = ($count_sequence->sequence + 1);
-                $create_img_multipla['image_name'] = $img_multipla;
-                ProductImage::create($create_img_multipla);
-            }
-        }
+        //         list($width_orig, $height_orig) = getimagesize($for_img_multipla);
+        //         $ratio_orig = $width_orig / $height_orig;
+        //         if ($width_max / $height_max > $ratio_orig) {
+        //             $width_max = $height_max * $ratio_orig; //----
+        //         } else {
+        //             $height_max = $width_max / $ratio_orig; //----
+        //         }
+        //         $img_multipla = Image::make($for_img_multipla)->resize($width_max, $height_max);
+        //         $img_name_m = Str::random() . '.' . $for_img_multipla->extension();
+        //         $img_multipla->save($originalPath . $img_name_m);
+
+        //         $create_img_multipla['product_id'] = $request->id;
+        //         $create_img_multipla['sequence']   = ($count_sequence->sequence + 1);
+        //         $create_img_multipla['image_name'] = $img_multipla;
+        //         ProductImage::create($create_img_multipla);
+        //     }
+        // }
 
         ProductCategory::where('product_id', $request->id)->delete();
 
@@ -262,36 +246,31 @@ class ProductController extends Controller
             }
         }
 
-        $product_image  = ProductImage::where('product_id', $request->id)->where('sequence', '1')->first();
-        $image          = Storage::get($product_image->image_name);
-        $mime_type      = Storage::mimeType($product_image->image_name);
-        $image          = 'data:' . $mime_type . ';base64,' . base64_encode($image);
 
-        $images_json = ProductImage::where('product_id', $request->id)->get();
-        $data_images = [];
-        foreach ($images_json as $imgjson) {
-            // Pegando a imagem e tarnsformamndo em data
-            $images         = Storage::get($imgjson->image_name);
-            $mime_types     = Storage::mimeType($imgjson->image_name);
-            $images         = 'data:' . $mime_types . ';base64,' . base64_encode($images);
+        // $product_image  = ProductImage::where('product_id', $request->id)->where('sequence', '1')->first();
+        // if (!empty($product_image)) {
+        //     $image          = Storage::get($product_image->image_name);
+        //     $mime_type      = Storage::mimeType($product_image->image_name);
+        //     $image          = 'data:' . $mime_type . ';base64,' . base64_encode($image);
+        //     return response()->json('oi');
+        //     $images_json = ProductImage::where('product_id', $request->id)->get();
+        //     $data_images = [];
+        //     \Log::info($images_json);
+        //     foreach ($images_json as $imgjson) {
+        //         // Pegando a imagem e tarnsformamndo em data
+        //         $images         = Storage::get($imgjson->image_name);
+        //         $mime_types     = Storage::mimeType($imgjson->image_name);
+        //         $images         = 'data:' . $mime_types . ';base64,' . base64_encode($images);
 
-            // Adiconando a um array para depois ser utilizados
-            $data_images[] = [
-                'sequence'  => $imgjson->sequence,
-                'image'     => $images
-            ];
-        }
+        //         // Adiconando a um array para depois ser utilizados
+        //         $data_images[] = [
+        //             'sequence'  => $imgjson->sequence,
+        //             'image'     => $images
+        //         ];
+        //     }
 
-        return response()->json([
-            'product_id' => $request->id,
-            'image_data' => $image,
-            'product_name' => mb_convert_case($request->name, MB_CASE_TITLE),
-            'sales_unit' => $this->sales_unit_array[$request->sales_unit],
-            'value' => 'R$ ' . $request->value,
-            'discount' => '',
-            // 'dados' => Product::where('id', $request->id)->with(['productImage'])->first(),
-            // 'dados_image' => $data_images
-        ], 200);
+        // }
+        return response()->json('oi');
     }
 
     public function inativarProduto(Request $request)
