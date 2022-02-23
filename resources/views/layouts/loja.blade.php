7ab11f83c2112a4e111fd75c68db0a93afc360a1
Felipe Cruz <felipephplow@gmail.com>
2022-02-23 16:38:21 -0300

    das

--- resources/views/layouts/loja.blade.php
+++ resources/views/layouts/loja.blade.php
@@ -18,7 +18,7 @@
                 <div class="row align-items-center">
                     <div class="col-md-4">
                         <div class="logo">
-                            <img src="{{ asset('assets/img/logo-dourada-nova.png') }}" alt="">
+                            <a href="{{ route('loja') }}"> <img src="{{ asset('assets/img/logo-dourada-nova.png') }}" alt=""></a>
                         </div>
                     </div>
                     <div class="col-md-4">
@@ -27,18 +27,21 @@
                                 <input type="search" class="form-control" placeholder="Buscar..."
                                     aria-label="Recipient's username" aria-describedby="button-addon2">
                                 <div class="input-group-append">
-                                    <button class="btn-search" type="button"
-                                        id="button-addon2"><i class="fas fa-search"></i></button>
+                                    <button class="btn-search" type="button" id="button-addon2"><i
+                                            class="fas fa-search"></i></button>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4">
-                        <div class="d-flex justify-content-center">
-                            <div>
-                                <i class="far fa-user"></i>
+                        <div class="d-flex justify-content-center icons">
+                            <div class="user">
+                                <a href=""> <i class="far fa-user"></i></a>
                             </div>
-                            <div>
+                            <div class="divider">
+
+                            </div>
+                            <div class="cart">
                                 <i class="fas fa-shopping-cart"></i>
                             </div>
 
@@ -47,15 +50,114 @@
                 </div>
             </div>
         </div>
-    </header>
-    <main>
+        <div class="shopping-cart">
+            <div class="cart-header">
+                <button class="cart-close"><i class="far fa-times-circle"></i></button>
+            </div>
+            <div class="cart-body">
+                <div class="mini-shopping-cart">
+                    <div class="mini-cart-item">
+                        <div>
+                            <img
+                                src="http://www.coolmix.nl/media/catalog/product/cache/1/thumbnail/75x/9df78eab33525d08d6e5fb8d27136e95/s/a/samsung_galaxy_s6_0003s_0002_att_black_600x600_xlarge_2_2.jpg">
+                        </div>
+                        <div>
+                            <span>Samsung Galaxy S6 Edge Refurbished 32 GB Black<br></span>
+                            <span>Artikelnummer: 103530<br>Aantal: 1</span>
+                        </div>
+                        <div>
+                            <span>€ 595,00<br></span>
+                            <i class="fa fa-trash-o"></i>
+                        </div>
+                    </div>
+                    <div class="mini-cart-item">
+                        <div>
+                            <img
+                                src="http://www.coolmix.nl/media/catalog/product/cache/1/thumbnail/75x/9df78eab33525d08d6e5fb8d27136e95/2/0/20460-1.jpg">
+                        </div>
+                        <div>
+                            <span>MountR Case Galaxy S5 Black<br></span>
+                            <span>Artikelnummer: 103530<br>Aantal: 1</span>
+                        </div>
+                        <div>
+                            <span>€ 34,95<br></span>
+                            <i class="fa fa-trash-o" alt="Remove item"></i>
+                        </div>
+                    </div>
+                    <div class="mini-cart-subtotal">
+                        <div>
+                            <span>Subtotaal:</span>
+                        </div>
+                        <div>
+                            <span>€ 629,95</span>
+                        </div>
+                    </div>
+                    <div class="mini-shopping-cart-buttons text-center">
+                        <button class="waves-effect waves-orange btn btn-dark">FINALIZAR COMPRA</button>
+                    </div>
 
-    </main>
-    <footer>
+                </div>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="navbar navbar-expand-md navbar-dark " role="navigation">
 
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link" href="#">Produtos </a>
+                        </li>
 
-</html>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="dropdown1" data-toggle="dropdown"
+                                aria-haspopup="true" aria-expanded="false">Categorias</a>
+                            <ul class="dropdown-menu" aria-labelledby="dropdown1">
+
+                                @forelse (getCategories() as $category)
+                                    <li class="dropdown-item  dropdown ">
+                                        <a class="@if ($category->subCategories->count() > 0) dropdown-toggle @endif"
+                                            id="dropdown1-1" data-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">{{ $category->name }}</a>
+                                        @if ($category->subCategories->count() > 0)
+                                            <ul class="dropdown-menu" aria-labelledby="dropdown1-1">
+                                                @forelse ($category->subCategories as $subCategory)
+                                                    <li class="dropdown-item" href="#">
+                                                        <a>{{ $subCategory->name }}</a>
+                                                    </li>
+                                                @empty
+                                                @endforelse
+                                            </ul>
+                                        @endif
+                                    </li>
+                                    @empty
+                                    @endforelse
+
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+
+            </div>
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="{{ asset('loja/js/script.js') }}"></script>
+    </body>
+
+    </html>
--- resources/views/loja/index.blade.php
+++ resources/views/loja/index.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.loja')
 
 @section('content')
-
-
     <!-- Slider Area -->
     <section class="hero-slider">
         <!-- Single Slider -->
         <div class="single-slider" style="background-image: url('assets/img/gmbrasil.jpg'); 	height: auto;
-                            background-size: cover;
-                            background-position: center;
-                            background-repeat: no-repeat;
-                            height: 510px;">
+                                                    background-size: cover;
+                                                    background-position: center;
+                                                    background-repeat: no-repeat;
+                                                    height: 510px;">
             <div class="container">
                 <div class="row no-gutters">
                     <div class="col-lg-9 offset-lg-3 col-12">
@@ -36,62 +34,26 @@
     <!-- Start Product Area -->
     <div class="product-area section">
         <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h2>Items</h2>
-                    </div>
-                </div>
-            </div>
+
             <div class="row">
                 <div class="col-12">
                     <div class="product-info">
                         <div class="nav-main">
-                            <!-- Tab Nav -->
-                            {{-- <ul class="nav nav-tabs" id="myTab" role="tablist">
-									<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#man" role="tab">Man</a></li>
-									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#women" role="tab">Woman</a></li>
-									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#kids" role="tab">Kids</a></li>
-									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#accessories" role="tab">Accessories</a></li>
-									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#essential" role="tab">Essential</a></li>
-									<li class="nav-item"><a class="nav-link" data-toggle="tab" href="#prices" role="tab">Prices</a></li>
-								</ul> --}}
-                            <!--/ End Tab Nav -->
+
                         </div>
-                        <div class="tab-content" id="myTabContent">
-                            <!-- Start Single Tab -->
-                            <div class="tab-pane fade show active" id="man" role="tabpanel">
-                                <div class="tab-single">
-                                    <div class="row">
-                                        @foreach ($products as $product)
-
-                                            <div class="col-xl-3 col-lg-4 col-md-4 col-12">
-                                                <div class="single-product">
-                                                    <div class="product-img">
-                                                        <a href="{{ route('product.detail', $product->id) }}">
-                                                            <img class="default-img" src="{{ asset('storage/' . $product->productImage[0]->image_name)}}" alt="#">
-                                                            <img class="hover-img" src="{{ asset('storage/' . $product->productImage[0]->image_name)}}" alt="#">
-                                                        </a>
-                                                        <div class="button-head">
-
-                                                            <div class="product-action-2">
-                                                                <button title="Add to cart" class="btnAdd" data-id="{{ $product->id }}" href="#">Adicionar ao Carrinho</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-content">
-                                                        <h3><a href="{{ route('product.detail', $product->id) }}">{{ $product->name }} </a></h3>
-                                                        <div class="product-price">
-                                                            <span>R$
-                                                                {{ number_format($product->value, 2, ',', '.') }}</span>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        @endforeach
-
+                        <div>
+                            <h1 class="text-center my-4 text-uppercase">Mais Vendidos</h1>
+                            <div class="container flexbox">
+                                @foreach ($products as $product)
+                                    <div class="product-item"><img
+                                            src="{{ asset('storage/' . $product->productImage[0]->image_name) }}">
+                                        <div class="product-list">
+                                            <h3>{{ $product->name }}</h3><span class="price">R$
+                                                {{ number_format($product->value, 2, ',', '.') }}</span><a href=""
+                                                class="button">ADICIONAR</a>
+                                        </div>
                                     </div>
-                                </div>
+                                @endforeach
                             </div>
 
                         </div>
@@ -107,49 +69,5 @@
     <!-- End Midium Banner -->
 
     <!-- Start Most Popular -->
-    <div class="product-area most-popular section">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title">
-                        <h2>Mais Comprados</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="owl-carousel popular-slider">
-                        <!-- Start Single Product -->
-                        @foreach ($products as $product)
-
-                            <div class="single-product">
-                                <div class="product-img">
-                                    <a href="{{ route('product.detail', $product->id) }}">
-                                        <img class="default-img" src="{{ asset('storage/' . $product->productImage[0]->image_name)}}" alt="#">
-                                        <img class="hover-img" src="{{ asset('storage/' . $product->productImage[0]->image_name)}}" alt="#">
-                                        <span class="out-of-stock">Hot</span>
-                                    </a>
-                                    <div class="button-head">->with(['productImage', 'productCategory'])
-
-                                        <div class="product-action-2">
-                                            <a title="Add to cart" href="#">Adicionar ao Carrinho</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="product-content">
-                                    <h3><a href="{{ route('product.detail', $product->id) }}">{{ $product->name }}</a></h3>
-                                    <div class="product-price">
-                                        <span>R$
-                                            {{ number_format($product->value, 2, ',', '.') }}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 @endsection
